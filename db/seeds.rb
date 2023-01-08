21109ca8db1e3c03359dfc04410e927a5118e4ef
Janice <janiceralecha@gmail.com>
2023-01-08 17:26:03 -0600

    Create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,9 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+savannah = Account.create(name:"Savannah Smith", email: "savsmith@outlook.com")
+
+Subdivision.create(name:"The Groves", community_type:"Luxury Homes")
+Subdivision.create(name:"Sienna", community_type:"Resort")
+Subdivision.create(name:"Lakeview", community_type:"Condo")
 puts "✅ Done seeding!"
