5bb8e7285c72f62930c83d60b4ec97210f1b38d2
Janice <janiceralecha@gmail.com>
2023-01-28 22:27:41 -0600

    Create CRUD methods to listings controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,43 +1,4 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/listings" do
-    listings = Listing.all
-    listings.to_json
-    listings.to_json(include: :subdivision)
-  end
-
-  post '/listings' do
-    listings = Listing.create(
-      image_url:params[:image_url],
-      status:params[:status],
-      list_price:params[:list_price],
-      year_built:params[:year_built],
-      storey:params[:storey],
-      bedroom:params[:bedroom],
-      bathroom:params[:bathroom],
-      garage:params[:garage],
-      building_size:params[:building_size],
-      lot_size:params[:lot_size],
-      architecture_style:params[:architecture_style],
-      pool:params[:pool],
-      subdivision_id:params[:subdivision_id],
-      account_id:params[:account_id])
-    listing.to_json
-  end
-  
-
-  patch '/listings/:id' do
-    listing = Listing.find(params [:id])
-    listings.update(list_price :params[:list_price]
-    )
-    listing.to_json
-  end
-
-  delete '/listings/:id' do
-    listing = Listing.find(params[:id])
-    listing.destroy
-    listing.to_json
-  end  
 end
