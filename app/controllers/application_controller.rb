42f8080988ef462fd2f239709ad8c79fe291b06c
Janice <janiceralecha@gmail.com>
2023-02-15 18:14:49 -0600

    Test and rerun seed file.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,25 +8,29 @@ class ApplicationController < Sinatra::Base
       subdivision = Subdivision.create(
           image_url: params[:image_url],
           name: params[:name],
-          community_type: params[:community_type],
           overview: params[:overview]
       )
       subdivision.to_json
       end
 
-  get "/listings" do
+    # Show all subdivision including their listings
+    get "/subdivisions" do
+    subdivision = Subdivision.all
+    subdivision.to_json(include: :listings)
+    end
+
+    get "/listings" do
     listings = Listing.all.order(:list_price)
     listings.to_json
     
    
-end
+    end
 
-# Add new listing
-post "/listing_form" do 
+    # Add new listing
+    post "/listing_form" do 
     listing = Listing.create(
         image_url: params[:image_url],
         active: true,
-        inactive: false,
         list_price: params[:list_price],
         year_built: params[:year_built],
         storey: params[:storey],
@@ -36,14 +40,13 @@ post "/listing_form" do
         building_size: params[:building_size],
         lot_size: params[:lot_size],
         architecture_style: params[:architecture_style],
-        pool: params[:pool],
         subdivision_id: params[:subdivision_id]
     )
     listing.to_json
-end
+    end
 
-# Update availability and listing price to a listing
-patch "/listings/:id" do 
+    # Update availability and listing price to a listing
+    patch "/listings/:id/edit" do 
     listing = Listing.find(params[:id])
     listing.update(
         list_price: params[:list_price],
@@ -51,20 +54,16 @@ patch "/listings/:id" do
         
     )
     listing.to_json
-end
+    end
 
-# Delete listing when availabilty is inactive
+    # Delete listing when availabilty is inactive
 
-delete "/listings/:id" do
+    delete "listings/:id/edit" do
     listing = Listing.find(params[:id])
     listing.destroy
     listing.to_json
-end 
+    end 
 
-# Show all subdivision including their listings
-get "/subdivisions" do
-  subdivision = Subdivision.all
-  subdivision.to_json(include: :listings)
-  end
 
-end
+
+    end
